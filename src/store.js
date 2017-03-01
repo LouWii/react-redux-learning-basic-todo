72dc3796a581234c67e418cb329c878f9fbae2b9
Louis DEKEISTER <louis@hopstudios.com>
2017-02-28 23:25:22 -0800

    add support for redux dev tools browser extension

--- src/store.js
+++ src/store.js
@@ -7,6 +7,6 @@ const defaultState = {
 	todos: [{text: "Run 5 km", complete: false}, {text: "Eat vegetables", complete: true}]
 }
 
-const store = createStore(rootReducer, defaultState)
+const store = createStore(rootReducer, defaultState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 export default store;
