16a4b92fe45f80f45ed8e675f5f5e09435ad8e7a
Louis D <link14.online@gmail.com>
2017-03-01 22:08:41 -0800

    add important comments to App component

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,10 @@ import * as actionCreators from '../actions/actionCreators';
 
 import Main from './Main';
 
+// If we were not using bindActionCreators, this App component would be useless,
+// We would have to call directly this.props.dispatch() with an action function in it
+// this.props.dispatch(addTodo("My new todo"))
+
 function mapStateToProps(state) {
   return {
     todos: state.todos
@@ -14,6 +18,7 @@ function mapDispatchToProps(dispatch) {
   return bindActionCreators(actionCreators, dispatch)
 }
 
+// App will link props directly to Main
 const App = connect(mapStateToProps, mapDispatchToProps)(Main);
 
 export default App;
